8a81180ac77be5fabf7a8a057917fe5f9b9b684e
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-10-04 16:39:00 -0300

    feature: Created Usuario

---
+++ src/main/java/com/rafael/TaskFlow/entity/Usuario.java
@@ -0,0 +1,38 @@
+package com.rafael.TaskFlow.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@NotEmpty
+	@Column(nullable = false, length = 100, unique = true)
+	private String email;
+	
+	@NotBlank
+	@NotEmpty
+	@Column(nullable = false, length = 16)
+	private String senha;
+
+}
