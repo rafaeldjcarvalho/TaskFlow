fdc2551fb2f6d4eaa287869b2c8fce3c714589bd
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-11-03 12:18:59 -0300

    feat: Create TarefaMapper

---
+++ src/main/java/com/rafael/TaskFlow/entity/dtos/mapper/TarefaMapper.java
@@ -0,0 +1,25 @@
+package com.rafael.TaskFlow.entity.dtos.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.rafael.TaskFlow.entity.Tarefa;
+import com.rafael.TaskFlow.entity.dtos.TarefaDTO;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class TarefaMapper {
+	
+	public TarefaDTO toDTO(Tarefa data) {
+		if(data == null) {
+			return null;
+		}
+		return new TarefaDTO(
+				data.getId(), 
+				data.getTitulo(), 
+				data.getDescricao(), 
+				data.getPrioridade(), 
+				data.getOrdem());
+	}
+}
